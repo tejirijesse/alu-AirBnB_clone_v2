fc6360602e613bda215938734eb9cca8cd4b60d1
tejirijesse <o.jesse1@alustudent.com>
2024-10-06 13:05:44 +0000

    Assignment Done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,14 +2,12 @@
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['52.1.22.58', '54.235.17.2']
+env.hosts = ['54.90.128.173', '10.110.24.148']
 
 
 def do_pack():
@@ -21,7 +19,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except BaseException:
         return None
 
 
@@ -42,7 +40,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except BaseException:
         return False
 
 
@@ -51,4 +49,4 @@ def deploy():
     archive_path = do_pack()
     if archive_path is None:
         return False
-    
+    return do_deploy(archive_path)
